a28984c4617f4da2fd3741ad8afe8b893cd29a14
nate <nate.stott@pm.me>
2025-10-21 20:22:10 -0600

    format

--- packages/server/src/main.rs
+++ packages/server/src/main.rs
@@ -6,9 +6,7 @@ use std::net::SocketAddr;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let addr = SocketAddr::from(([0, 0, 0, 0], 8000));
-    let make_svc = make_service_fn(|_conn| {
-        async { Ok::<_, Infallible>(service_fn(hello_world)) }
-    });
+    let make_svc = make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(hello_world)) });
     let server = Server::bind(&addr).serve(make_svc);
     println!("Server listening on http://{}", addr);
     server.await?;
@@ -20,7 +18,7 @@ async fn hello_world(_req: Request<Body>) -> Result<Response<Body>, Infallible> 
     *response.status_mut() = StatusCode::OK;
     *response.body_mut() = Body::from("Hello, World!");
     Ok(response)
-}   
+}
 
 #[cfg(test)]
 mod tests {
